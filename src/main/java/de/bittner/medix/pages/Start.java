55e115a267f812c76c544c9a9c9bd9376e74f64f
Winfried Bittner <winfried.bittner@gmail.com>
2016-01-06 23:49:26 +0100

    example directory listing implemented in wicket startpage

--- src/main/java/de/bittner/medix/pages/Start.java
+++ src/main/java/de/bittner/medix/pages/Start.java
@@ -1,8 +1,21 @@
 package de.bittner.medix.pages;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.tag.FieldKey;
+import org.jaudiotagger.tag.TagException;
 
 import de.bittner.medix.ui.BasePage;
 
@@ -16,10 +29,42 @@ public class Start extends BasePage {
 
 	@Override
 	protected void onInitialize() {
-		// TODO Auto-generated method stub
 		super.onInitialize();
 		Date date = new Date();
 		add(new Label("message", "Hello Medix! - " + date.toString()));
+
+		String testFileUrl = "/home/wbittner/Musik/Bravo Hits, Vol. 91";
+		File dir = new File(testFileUrl);
+		List<File> fileList = Arrays.asList(dir.listFiles());
+		fileList.sort((File a, File b) -> {
+			try {
+				org.jaudiotagger.tag.Tag tagA = getTagFromFile(a);
+				org.jaudiotagger.tag.Tag tagB = getTagFromFile(b);
+				return Integer.parseInt(tagA.getFirst(FieldKey.TRACK))
+						- Integer.parseInt(tagB.getFirst(FieldKey.TRACK));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return -1;
+		});
+		ListView<File> listView = new ListView<File>("listview", fileList) {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected void populateItem(ListItem<File> file) {
+				try {
+					org.jaudiotagger.tag.Tag tag = getTagFromFile(file.getModelObject());
+					file.add(new Label("label", tag.getFirst(FieldKey.TRACK) + " - " + tag.getFirst(FieldKey.TITLE)));
+				} catch (CannotReadException | IOException | TagException | ReadOnlyFileException
+						| InvalidAudioFrameException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+		};
+		add(listView);
 	}
 
 	@Override
@@ -27,4 +72,10 @@ public class Start extends BasePage {
 		return "Start";
 	}
 
+	private org.jaudiotagger.tag.Tag getTagFromFile(File file)
+			throws CannotReadException, IOException, TagException, ReadOnlyFileException, InvalidAudioFrameException {
+		AudioFile audioFile = AudioFileIO.read(file);
+		org.jaudiotagger.tag.Tag tag = audioFile.getTag();
+		return tag;
+	}
 }
